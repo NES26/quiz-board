98aaf3e5dc91e09d117a19bdb2c04720a6a3b07f
nirnuhsimel@gmail.com <nirnuhsimel@gmail.com>
2019-04-01 11:27:10 +0300

    Add scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,64 +1,35 @@
 
   
-var quizResults = function(one, two, three, four, five, six) {
-  return one + two + three + four;
+function check() {
+  var questionOne = document.Quiz.questionOne.value;
+  var questionTwo = document.Quiz.questionTwo.value;
+  var questionThree = document.Quiz.questionThree.value;
+  var questionFour = document.Quiz.questionFour.value;
+  var right = 0;
+
+
+  if (questionOne == "correct") {
+      right = right + 20;
+  };
+  if (questionTwo == "correct") {
+      right = right + 20;
+  };
+  if (questionThree == "correct") {
+      right = right + 20;
+  };
+  if (questionFour == "correct") {
+      right = right + 20;
+  };
+  if (questionFive == "correct") {
+      right = right + 20;
+  };
+
+
+
+  document.getElementById("fifth-p").style.visibility = "visible";
+  document.getElementById("correctNumber").innerHTML = "HERE IS YOUR SCORE: " + right + "%";
+  $(".container").fadeOut();
+
 };
 
-$(document).ready(function() {
-  $("#btn1").click(function() {
-    $("#form1").show();
 
-  });
-
-  $(".initially-showing").submit(function(event) {
-    var Question1 = parseInt($("input:radio[name=Question1]:checked").val());
-    if ($("input:radio[name=Question1]:checked").val() === undefined) {
-      $("#q1").text("Please give an answer");
-      return false;
-    }
-    var Question2 = parseInt($("input:radio[name=Question2]:checked").val());
-    if ($("input:radio[name=Question2]:checked").val() === undefined) {
-      $("#q2").text("Please give an answer");
-      return false;
-    }
-    var Question3 = parseInt($("input:radio[name=Question3]:checked").val());
-    if ($("input:radio[name=Question3]:checked").val() === undefined) {
-      $("#q3").text("Please give an answer");
-      return false;
-    }
-    var Question4 = parseInt($("input:radio[name=Question4]:checked").val());
-    if ($("input:radio[name=Question4]:checked").val() === undefined) {
-      $("#q4").text("Please give an answer");
-      return false;
-    }
-    var Question5 = parseInt($("input:radio[name=Question5]:checked").val());
-    if ($("input:radio[name=Question5]:checked").val() === undefined) {
-      $("#q5").text("Please give an answer");
-      return false;
-    }
-    var Question6 = parseInt($("input:radio[name=Question6]:checked").val());
-    if ($("input:radio[name=Question6]:checked").val() === undefined) {
-      $("#q6").text("Please give an answer");
-      return false;
-    }
-    var result = quizResults(
-      Question1,
-      Question2,
-      Question3,
-      Question4,
-    );
-    var resultScore = parseInt(quiz1)+parseInt(quiz2)+parseInt(quiz3)+parseInt(quiz4);
-    $("#finish").text("You SCored: " + resultScore + " out of 100");
-
-
-    $(".display").text("You scored:" + result + " /30");
-
-    $(".finalResults").show();
-
-    event.preventDefault();
-    $(".initially-showing").fadeOut();
-  });
-  $("#reset").click(function() {
-    location.reload(); 
-  });
-});
